45c63046d159e672a4864190c6f0478dd1a1889b
Nikolai Sazonov <nsazonov9@gmail.com>
2025-01-08 13:32:21 -0500

    Initial Commit

---
+++ LetsLearnChineseTests/LetsLearnChineseTests.swift
@@ -0,0 +1,17 @@
+//
+//  LetsLearnChineseTests.swift
+//  LetsLearnChineseTests
+//
+//  Created by Dmitry Sazonov on 1/8/25.
+//
+
+import Testing
+@testable import LetsLearnChinese
+
+struct LetsLearnChineseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
