b28e4228d7a34d806416ca58c5e7345bba03e433
Nikolai Sazonov <nsazonov9@gmail.com>
2025-01-16 20:04:26 -0500

    Cleaned Up Unused Views

--- LetsLearnChinese/Views/DefaultBackground.swift
+++ LetsLearnChinese/Views/DefaultBackground.swift
@@ -33,3 +33,61 @@ struct DefaultBackground: View {
             .edgesIgnoringSafeArea(.bottom)
     }
 }
+
+struct TopBar: View {
+    var title: String = "Let's Learn"
+    var subtitle: String = "Chinese"
+    
+    var body: some View {
+        Group {
+            VStack(spacing: -50) { // Title and subtitle stack
+                Group {
+                    StrokedText(
+                        text: title,
+                        font: .custom("InknutAntiqua-Light", size: 26),
+                        strokeColor: .black,
+                        strokeWidth: 2,
+                        fillColor: .accent
+                    )
+                    
+                    StrokedText(
+                        text: subtitle,
+                        font: .custom("InknutAntiqua-Bold", size: 40),
+                        strokeColor: .black,
+                        strokeWidth: 2,
+                        fillColor: .accent
+                    )
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .top)
+            .background(Color.background)
+            .shadow(color: .black.opacity(0.55), radius: 5, x: 4, y: 5)
+        }
+        .frame(height: 113, alignment: .top)
+    }
+}
+
+struct BottomBar: View {
+    var body: some View {
+        VStack {
+//            Spacer()
+            ZStack {
+                // Inner shadow
+                Rectangle()
+                    .fill(Color.black.opacity(0.75))
+                    .padding(2) // Padding for the inner shadow effect
+                    .frame(height: 34)
+                    .frame(maxWidth: .infinity)
+                    .blur(radius: 5) // Adds the blur to the inner shadow
+                
+                // Outer box with shadow
+                Rectangle()
+                    .fill(Color.background)
+                    .frame(height: 34)
+                    .frame(maxWidth: .infinity)
+                    .shadow(color: .black.opacity(0.25), radius: 5, x: 0, y: -5) // Outer shadow
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
