cfc9f3bb6fa9711384387b73bcc00f603187b175
Nikolai Sazonov <nsazonov9@gmail.com>
2025-01-21 09:48:12 -0500

    Testing For GetRand Character from Unit

--- LetsLearnChinese/Views/DBView.swift
+++ LetsLearnChinese/Views/DBView.swift
@@ -12,6 +12,38 @@ struct DBView: View {
     @Environment(\.modelContext) private var modelContext
 
     var body: some View {
-        Text("Hello")
+        VStack {
+            Text("Hello")
+            
+            Button("Press me to print random character in unit 1"){
+                // Example usage
+                let randomCharacter = DataBaseManager.shared.getRandomCharacter(fromUnits: [1])
+                if let randomCharacter = randomCharacter {
+                    print(randomCharacter)
+                } else {
+                    print("No character found.")
+                }
+
+            }
+            
+            Button("Press me to print random character in unit 2"){
+                let randomCharacter = DataBaseManager.shared.getRandomCharacter(fromUnits: [2])
+                if let randomCharacter = randomCharacter {
+                    print(randomCharacter)
+                } else {
+                    print("No character found.")
+                }
+            }
+            
+            Button("Press me to print random character in unit 3"){
+                let randomCharacter = DataBaseManager.shared.getRandomCharacter(fromUnits: [3])
+                if let randomCharacter = randomCharacter {
+                    print(randomCharacter)
+                } else {
+                    print("No character found.")
+                }
+            }
+        }
+        
     }
 }
