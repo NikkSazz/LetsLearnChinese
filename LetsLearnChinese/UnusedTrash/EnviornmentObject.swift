b3431ba68503caa0151898b42eb8334c318c503a
Nikolai Sazonov <nsazonov9@gmail.com>
2025-01-23 07:33:38 -0500

    Deleted Trash

---
+++ LetsLearnChinese/UnusedTrash/EnviornmentObject.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import SwiftData
+
+struct EnviornmentObject: View {
+    var body: some View {
+        ZStack {
+            DefaultBackground()
+            
+            
+        }
+    }
+}
