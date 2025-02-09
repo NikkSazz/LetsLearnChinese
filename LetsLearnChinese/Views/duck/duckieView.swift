8c105041d7a59c5ca103cba160bdc516e82f2b16
Nikolai Sazonov <nsazonov9@gmail.com>
2025-02-09 18:36:59 -0500

    Red Duck

--- LetsLearnChinese/Views/duck/duckieView.swift
+++ LetsLearnChinese/Views/duck/duckieView.swift
@@ -18,10 +18,11 @@ struct duckieView: View {
     @State var duckType: Int = 0
     
     let ducks = [
-        Model(id: 0, name: "Rubber", modelName: "duck.obj", textureName: "duck.png"),
-        Model(id: 1, name: "Green", modelName: "greenduck.obj", textureName: "greenduck.png"),
-//        Model(id: 2, name: "Traveler ", modelName: "duck_fin_posed.obj", textureName: "greenduck.png"),
-//        Model(id: 3, name: "Wooden", modelName: "wooden_duck_toy.obj", textureName: "greenduck.png")
+        Duck(id: 0, name: "Rubber", modelName: "duck.obj", textureName: "duck.png"),
+        Duck(id: 1, name: "Red", modelName: "duck.obj", textureName: "redduck.png"),
+        Duck(id: 2, name: "Green", modelName: "greenduck.obj", textureName: "greenduck.png"),
+//        Duck(id: 3, name: "Traveler ", modelName: "duck_fin_posed.obj", textureName: "greenduck.png"),
+//        Duck(id: 4, name: "Wooden", modelName: "wooden_duck_toy.obj", textureName: "greenduck.png")
         ]
     var body: some View{
         ZStack{
@@ -146,7 +147,7 @@ struct duckieView: View {
 }
 
 // Data Model...
-struct Model : Identifiable {
+struct Duck : Identifiable {
     
     var id : Int
     var name : String
