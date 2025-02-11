b5acb8b3a9078bf03cd5eb84adc1b9677bf34e21
Nikolai Sazonov <nsazonov9@gmail.com>
2025-02-11 15:50:22 -0500

    Cool animation with expanded stuff if button is pressed

--- LetsLearnChinese/Views/DictionarySingleCharacterView.swift
+++ LetsLearnChinese/Views/DictionarySingleCharacterView.swift
@@ -11,8 +11,8 @@ struct DictionarySingleCharacterView: View {
     
     let character: Character
     
-    @State private var oldExpandedEachChar = false
-    @State private var expandedEachChar = false
+    @State private var oldExpandedEachChar = false // delete when removing the old version
+//    @State private var expandedEachChar = false // redundant due to expandedChar
     @State private var expandedChar = "я"
     
     
@@ -22,70 +22,22 @@ struct DictionarySingleCharacterView: View {
             
             VStack(spacing: 10) {
                     
-                Text(character.chinese)
-                    .font(.system(size: 65))
-                    .padding()
-                    .background(
-                        LinearGradient(gradient: Gradient(colors: [.cyan.opacity(0.2), .purple.opacity(0.2)]),
-                                       startPoint: .bottomLeading,
-                                       endPoint: .topTrailing)
-                        .cornerRadius(10)
-                    ) // background
-                    .shadow(color: .black, radius: 3, x: -5, y: 5)
-//                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
                 
+                coolDictText(character)
                 
-                // Old
-                Button {
-                    withAnimation {
-                        oldExpandedEachChar.toggle()
-                    }
-                } label: {
-                    HStack() {
-                        Text("\(character.id)")
-                        Text("\(character.english)")
-                            .padding(.leading)
-                        
-                        Spacer()
-                        
-                        VStack(alignment: .trailing) {
-                            Text("\(character.chinese)")
-                            Text("\(character.pinyin)")
-                        }
-                        .multilineTextAlignment(.trailing)
-                        
-                    } // h
-                    .foregroundStyle(.accent)
-                    .frame(height: 50)
-                    .padding(.horizontal, 30) // inner padding
-                    .padding(.vertical, 2)
-                    .background(oldExpandedEachChar ?
-                                Color.black.opacity(0.4) : Color.gray.opacity(0.2))
-                    .cornerRadius(10)
-                    .padding(.horizontal, 30) // outer padding
-                } // expandedEachChar Button
-                
-                if oldExpandedEachChar {
-                    VStack {
-                        ForEach(character.chinese.map { String($0) }, id: \.self) { char in
-//                            let charAsString = String(char)
-                            Text(char)
-                        }
-                    } // v
-                } // if expandedEachChar
-                // \Old
-                
-                
+                oldThing(character: character)
                 
                 //New
                 ForEach(character.chinese.map { String($0) }, id: \.self) { char in
                     Button {
-                        expandedEachChar.toggle()
-                        expandedChar = expandedChar == char ? "я" : char
+                        withAnimation {
+                            //                        expandedEachChar.toggle()
+                            expandedChar = expandedChar == char ? "я" : char
+                        } // withAnimation
                     } label: {
                         
                         HStack {
-                            Text("More Words with \(char)")
+                            Text("More Words with")
                             
                             Text(char)
                                 .font(.system(size: 30))
@@ -93,7 +45,7 @@ struct DictionarySingleCharacterView: View {
                         .frame(height: 50)
                         .padding(.horizontal, 30)
                         .padding(.vertical, 2)
-                        .background(expandedEachChar &&
+                        .background(/*expandedEachChar &&*/
                                     expandedChar == char ?
                                     Color.black.opacity(0.4) : Color.gray.opacity(0.2))
                         .cornerRadius(10)
@@ -101,18 +53,97 @@ struct DictionarySingleCharacterView: View {
                         
                         Spacer() // move text to leading
                         
-                    }
+                    } // button label
 
+                    if expandedChar == char {
+                        VStack{
+                            Text("Hello")
+                            Text("Hello2")
+                            Text("Hello3")
+                            Text("Hello4")
+                        }
+                        .transition(.opacity.combined(with: .move(edge: .trailing)))
+                    } // if expandedChar == char
                     
                 } // for each char in Dictionary Character
                 // \new
                 Text("\(expandedChar)")
+                    .font(.system(size: 50))
                 
             } // v
             .foregroundStyle(.accent)
         } // Z
     } // body some view
 } // dictionary Signle Character View struct
+
+struct coolDictText: View {
+    let character: Character
+    
+    init(_ character: Character) {
+        self.character = character
+    } // makes things cleaner while coding
+    
+    var body: some View {
+        Text(character.chinese)
+            .font(.system(size: 65))
+            .padding()
+            .background(
+                LinearGradient(gradient: Gradient(colors: [.cyan.opacity(0.2), .purple.opacity(0.2)]),
+                               startPoint: .bottomLeading,
+                               endPoint: .topTrailing)
+                .cornerRadius(10)
+            ) // background
+            .shadow(color: .black, radius: 3, x: -5, y: 5)
+//                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+    }
+}
+
+struct oldThing: View {
+    @State var oldExpandedEachChar = false
+    let character: Character
+    
+    var body: some View {
+        //old
+        Button {
+            withAnimation {
+                oldExpandedEachChar.toggle()
+            }
+        } label: {
+            HStack() {
+                Text("\(character.id)")
+                Text("\(character.english)")
+                    .padding(.leading)
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(character.chinese)")
+                    Text("\(character.pinyin)")
+                }
+                .multilineTextAlignment(.trailing)
+                
+            } // h
+            .foregroundStyle(.accent)
+            .frame(height: 50)
+            .padding(.horizontal, 30) // inner padding
+            .padding(.vertical, 2)
+            .background(oldExpandedEachChar ?
+                        Color.black.opacity(0.4) : Color.gray.opacity(0.2))
+            .cornerRadius(10)
+            .padding(.horizontal, 30) // outer padding
+        } // expandedEachChar Button
+        
+        if oldExpandedEachChar {
+            VStack {
+                ForEach(character.chinese.map { String($0) }, id: \.self) { char in
+//                            let charAsString = String(char)
+                    Text(char)
+                }
+            } // v
+        } // if expandedEachChar
+        // \old
+    }
+}
 
 #Preview {
 //    DictionarySingleCharacterView(character: Character(id: 9, chinese: "小姐", english: "miss", pinyin: "xiaojie"))
